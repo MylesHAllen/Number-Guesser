03266f93ca96b03e0d85f645dcc652d0ce0563d5
Myles.Allen <mylesharrisonallen@icloud.com>
2024-06-06 17:34:36 +0100

    Project Complete
    index.js:
    - Added a try count to the if statment that
    prints out the number of tries it took the
    user before they got the correct number.
    
    style.css:
    - Added a margin-left: 40px; for <p>.
    - Added a margin-right: 20px; for <p>.
    - Changed background colour of .container to
    rgb(176, 225, 226);
    - label font size has been added to equal 1.2em.
    - Added input text-align: center;.
    - Changed the input box width to 70px.
    - Added button:active background-color:
    rgb(86, 145, 163);.
    - Added button:hover background-color:
    rgb(144, 206, 224);.
    - Added cursor: pointer; to button:hover.

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ console.log(num);
 // Variables declared for use in function below
 let myBtn = document.getElementById("btn")
 let guess;
+let tries = 0;
 
 // Generates a message based on the users input as to how close they are to the number.
 myBtn.onclick = function(){
     guess = document.getElementById("inpt").value;
     if (guess < num){
         document.getElementById("message").textContent = ("Your guess is too low, try again!")
+        tries = tries + 1;
     }
     else if (guess > num){
         document.getElementById("message").textContent = ("Your guess is too high, try again!")
+        tries = tries + 1;
     }
     else{
-        document.getElementById("message").textContent = ("Well done you guessed right!")
+        document.getElementById("message").textContent = (`Well done you guessed right! It took you ${tries} tries.`)
     }
 }
 
